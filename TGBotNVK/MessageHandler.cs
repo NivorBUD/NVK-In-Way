b793a2d6134387e9e8908e394312179d886cc6f8
Dubrovin Vyacheslav <via4eslav.dubrovin@yandex.ru>
2024-12-15 20:34:52 +0500

    Не идеальная логика для водителя

---
+++ TGBotNVK/MessageHandler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TGBotNVK;
+
+public static class MessageHandler
+{
+    public static async void ProcessMessage(Message msg, ITelegramBotClient botClient)
+    {
+        var user = msg.From;
+        var chat = msg.Chat;
+
+        switch (msg.Type)
+        {
+            case MessageType.Text:
+                {
+                    if (msg.Text == "/start")
+                    {
+                        await PrintStartMenu(botClient, chat);
+                        return;
+                    }
+                    return;
+                }
+            default:
+                {
+                    await botClient.SendMessage(
+                        chat.Id,
+                        "Используй только текст!");
+                    return;
+                }
+        }
+    }
+
+    public static async Task PrintStartMenu(ITelegramBotClient botClient, Chat chat)
+    {
+        var inlineKeyboard = new InlineKeyboardMarkup(
+            new List<InlineKeyboardButton[]>()
+                {
+                    new InlineKeyboardButton[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("Создать профиль", "create profile")
+                    },
+                    new InlineKeyboardButton[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("Посмотреть список созданных поездок", "check trips")
+                    },
+                    new InlineKeyboardButton[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("Создать поездку", "create trip")
+                    },
+                    new InlineKeyboardButton[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("Редактировать профиль", "change profile")
+                    }
+                });
+
+        await botClient.SendMessage(
+            chat.Id, "Выберите действие",
+            replyMarkup: inlineKeyboard);
+    }
+
+    public static async void CheckStartChoice(ITelegramBotClient botClient, Update update)
+    {
+        if (Program.isBusy) return;
+
+        var callbackQuery = update.CallbackQuery;
+        var user = callbackQuery.From;
+        var chat = callbackQuery.Message.Chat;
+
+        switch (callbackQuery.Data)
+        {
+            case "create profile":
+                {
+                    await botClient.AnswerCallbackQuery(callbackQuery.Id);
+                    Program.isBusy = true;
+                    ProfileHandler.StartCreatingDriverProfile(callbackQuery.Message, botClient);
+                    return;
+                }
+            case "check trips":
+                {
+                    await botClient.AnswerCallbackQuery(callbackQuery.Id);
+                    await botClient.SendMessage(chat.Id, ProfileHandler.GetDriverTrips(user.Id));                    
+                    return;
+                }
+            case "create trip":
+                {
+                    await botClient.AnswerCallbackQuery(callbackQuery.Id);
+                    ProfileHandler.CreateTrip(callbackQuery.Message, botClient);
+                    return;
+                }
+            case "change profile":
+                {
+                    return;
+                }
+        }
+        return;
+    }
+}
---
+++ TGBotNVK/ProfileHandler.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using static Telegram.Bot.TelegramBotClient;
+using Telegram.Bot.Polling;
+
+namespace TGBotNVK;
+public static class ProfileHandler
+{
+    private static int creatingProfileStep;
+    private static int creatingTripStep;
+
+    public static async void StartCreatingDriverProfile(Message msg, ITelegramBotClient botClient)
+    {
+        if (!Program.DriversDataBase.TryGetValue(msg.Chat.Id, out var driver))
+        {
+            driver = new Driver();
+            Program.DriversDataBase[msg.Chat.Id] = driver;
+        }
+
+        if (driver.IsProfileComplete)
+        {
+            await botClient.SendMessage(msg.Chat.Id, "Ваш профиль водителя заполнен");
+            Program.isBusy = false;
+            return;
+        }
+
+        await botClient.SendMessage(
+                        msg.Chat.Id,
+                        "Введите марку авто");
+
+        Program.StartBotWithAnotherUpdateHandler(CreateDriverProfile);
+    }
+
+    private static async Task CreateDriverProfile(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var msg = update.Message;
+        var chat = msg.Chat;
+        var driver = Program.DriversDataBase[msg.From.Id];
+
+        switch (creatingProfileStep)
+        {
+            case 0:
+                {
+                    driver.SetAutoName(msg.Text);
+                    driver.SetTGId(msg.From.Id);
+                    await botClient.SendMessage(
+                        chat.Id, 
+                        "Введите номер авто", 
+                        cancellationToken: cancellationToken);
+                    creatingProfileStep++;
+                    return;
+                }
+            case 1:
+                {
+                    driver.SetAutoNumber(msg.Text);
+                    await botClient.SendMessage(
+                        chat.Id, 
+                        "Введите цвет авто", 
+                        cancellationToken: cancellationToken);
+                    creatingProfileStep++;
+                    return;
+                }
+            case 2:
+                {
+                    driver.SetAutoColor(msg.Text);
+                    Program.isBusy = false;
+                    creatingProfileStep = 0;
+                    await botClient.SendMessage(
+                        chat.Id, 
+                        driver.ToString(), 
+                        cancellationToken: cancellationToken);
+                    Program.StartWithStandardUpdateHandler();
+                    return;
+                }
+        }
+    }
+
+    public static string GetDriverTrips(long driverTGId)
+    {
+        var driver = Program.DriversDataBase[driverTGId];
+
+        return driver.GetTrips();
+    }
+
+    public static async void CreateTrip(Message msg, ITelegramBotClient botClient)
+    {
+        if (!Program.DriversDataBase.TryGetValue(msg.Chat.Id, out var driver))
+        {
+            driver = new Driver();
+            Program.DriversDataBase[msg.Chat.Id] = driver;
+        }
+
+        if (!driver.IsProfileComplete)
+        {
+            await botClient.SendMessage(msg.Chat.Id, "Ваш профиль водителя не заполнен, заполните его");
+            Program.isBusy = false;
+            return;
+        }
+
+        await botClient.SendMessage(
+                        msg.Chat.Id,
+                        "Введите точку отправления");
+
+        Program.StartBotWithAnotherUpdateHandler(CreatingTrip);
+    }
+
+    private static async Task CreatingTrip(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var msg = update.Message;
+        var chat = msg.Chat;
+        var driver = Program.DriversDataBase[msg.From.Id];
+        driver.CreatedTrip = driver.CreatedTrip == null ? new Trip(driver) : driver.CreatedTrip;
+
+        switch (creatingProfileStep)
+        {
+            case 0:
+                {
+                    driver.SetAutoName(msg.Text);
+                    driver.SetTGId(msg.From.Id);
+                    await botClient.SendMessage(
+                        chat.Id,
+                        "Введите номер авто",
+                        cancellationToken: cancellationToken);
+                    creatingProfileStep++;
+                    return;
+                }
+            case 1:
+                {
+                    driver.SetAutoNumber(msg.Text);
+                    await botClient.SendMessage(
+                        chat.Id,
+                        "Введите цвет авто",
+                        cancellationToken: cancellationToken);
+                    creatingProfileStep++;
+                    return;
+                }
+            case 2:
+                {
+                    driver.SetAutoColor(msg.Text);
+                    Program.isBusy = false;
+                    creatingProfileStep = 0;
+                    await botClient.SendMessage(
+                        chat.Id,
+                        driver.ToString(),
+                        cancellationToken: cancellationToken);
+                    Program.StartWithStandardUpdateHandler();
+                    return;
+                }
+        }
+    }
+}
+
+
