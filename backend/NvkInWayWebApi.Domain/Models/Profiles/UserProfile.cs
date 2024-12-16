a59015bd18cadf38c6f6b2477ced1a4d579008d2
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-12-16 22:59:21 +0500

    Начальное добавление моделей

---
+++ backend/NvkInWayWebApi.Domain/Models/Profiles/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NvkInWayWebApi.Domain.Models.Profiles
+{
+    public abstract class UserProfile
+    {
+        public Guid Id { get; set; }
+
+        public long TgProfileId { get; set; }
+
+        public int Rating { get; set; }
+
+        public int TripsCount { get; set; }
+    }
+}
---
+++ backend/NvkInWayWebApi.Domain/Models/Trip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NvkInWayWebApi.Domain.Models
+{
+    public class Trip
+    {
+        public Guid Id { get; set; }
+
+        public Guid DriverId { get; set; }
+
+        public Location StartPoint { get; set; }
+
+        public Location EndPoint { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public int TotalPlaces { get; set; }
+
+        public int BookedPlaces { get; set; }
+
+        public double Cost { get; set; }
+
+        public string CarLocation { get; set; }
+
+        public Car DriverCar { get; set; }
+    }
+}
