7bc36332e975bb522d042b018d29dcfce6afeef2
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2024-12-17 17:58:14 +0500

    add passenger repository

---
+++ backend/NvkInWayWebApi.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace NvkInWayWebApi.Persistence.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(string id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveChangesAsync();
+    }
+}
