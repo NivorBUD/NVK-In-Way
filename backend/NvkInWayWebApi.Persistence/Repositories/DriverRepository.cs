f60b090360ddc93399c6a037e83c260308af207e
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-12-18 10:51:29 +0500

    implemented part of the driver repository

---
+++ backend/NvkInWayWebApi.Persistence/Repositories/DriverRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NvkInWayWebApi.Persistence.Entities;
+
+namespace NvkInWayWebApi.Persistence.Repositories
+{
+    public class DriverRepository : CommonRepository<Driver>
+    {
+        public DriverRepository(DbContext context) : base(context) { }
+
+        public async Task AddDriverAsync(Driver driver)
+        {
+            await AddAsync(driver);
+            await SaveChangesAsync();
+        }
+
+        public async Task UpdateDriverAsync(Driver driver)
+        {
+            Update(driver);
+            await SaveChangesAsync();
+        }
+
+        public async Task DeleteDriverAsync(string id)
+        {
+            var driver = await GetByIdAsync(id);
+            if (driver != null)
+            {
+                Delete(driver);
+                await SaveChangesAsync();
+            }
+        }
+    }
+}
